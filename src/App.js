4a5cc02f2f7513b7fead52417e7df6503dfd6ea3
Penelope Durand <penelope.durand@mail.goucher.edu>
2022-06-01 13:21:46 -0400

    displaying worlds

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Worlds from './Components/Worlds'
 
 function App() {
   return (
     <div className="App">
       <h1> heyyyyyy </h1>
+      <Worlds />
     </div>
   );
 }
--- src/Components/Worlds.js
+++ src/Components/Worlds.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import World from './World'
 
-function worlds() {
+function Worlds() {
+  const [worlds, setWorlds] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:9292/worlds/')
+      .then(resp => resp.json())
+      .then(data => setWorlds(data))
+  }, [])
+
+  // console.log(worlds)
+
   return (
-    <div>worlds</div>
+    <>
+      <div>worlds</div>
+      {worlds.map((world) => (
+        <World key={world.id} world={world} />
+      ))}
+    </>
   )
 }
 
-export default worlds
+export default Worlds
